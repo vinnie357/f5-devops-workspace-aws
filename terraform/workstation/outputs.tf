fcdd0a002985b64f799814c6c9bd7c91aca4c699
vinnie <vinnie.mazza@gmail.com>
2020-03-30 14:34:13 -0400

    less cool ip gathering

--- terraform/workstation/outputs.tf
+++ terraform/workstation/outputs.tf
@@ -1,8 +1,13 @@
+# output "workspaceManagementAddress" {
+#   # Result is a map from instance id to private IP address, such as:
+#   #  {"i-1234" = "192.168.1.2", "i-5678" = "192.168.1.5"}
+#   value = {
+#     for instance in aws_instance.workstation:
+#     instance.id => "ssh@${aws_instance.workstation.public_ip}"
+#   }
+# }
 output "workspaceManagementAddress" {
   # Result is a map from instance id to private IP address, such as:
   #  {"i-1234" = "192.168.1.2", "i-5678" = "192.168.1.5"}
-  value = {
-    for instance in aws_instance.workstation:
-    instance.id => "ssh@${aws_instance.workstation.public_ip}"
-  }
+  value = {"ssh@${aws_instance.workstation.public_ip}"}
 }
