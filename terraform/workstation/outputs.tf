a0a4cb5e9b44bb8bd68163d4c13ed1acfc905fac
vinnie <vinnie.mazza@gmail.com>
2020-03-30 14:17:33 -0400

    eip

--- terraform/workstation/outputs.tf
+++ terraform/workstation/outputs.tf
@@ -3,6 +3,6 @@ output "workspaceManagementAddress" {
   #  {"i-1234" = "192.168.1.2", "i-5678" = "192.168.1.5"}
   value = {
     for instance in aws_instance.workstation:
-    instance.name => "ssh@${instance.network_interface.0.access_config.0.nat_ip}"
+    instance.name => "ssh@${aws_eip.mgmt.public_ip}"
   }
 }
