912d78555603efee3847fe025ca32abef76e539b
vinnie <vinnie.mazza@gmail.com>
2020-03-30 14:32:49 -0400

    instance name to id

--- terraform/workstation/outputs.tf
+++ terraform/workstation/outputs.tf
@@ -3,6 +3,6 @@ output "workspaceManagementAddress" {
   #  {"i-1234" = "192.168.1.2", "i-5678" = "192.168.1.5"}
   value = {
     for instance in aws_instance.workstation:
-    instance.name => "ssh@${aws_instance.workstation.public_ip}"
+    instance.id => "ssh@${aws_instance.workstation.public_ip}"
   }
 }
